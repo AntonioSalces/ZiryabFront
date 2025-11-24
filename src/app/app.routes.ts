83c1136579c966a2def13910f9f16dde7bd2410a
Paco16889 <francisco.cobsan@gmail.com>
2025-11-24 19:31:07 +0100

    subo rama con version 19 d angular/fire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
     {
         path:'dashboard',
         component:DashboardComponent
-        //canActivate:[authGuard]
+        canActivate:[authGuard]
     },
     {
         path:'clases',
